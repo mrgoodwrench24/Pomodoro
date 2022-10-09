804a9aee7387259cde9d74f1d0af668520db7f74
mrgoodwrench24 <michaeltroywhite24@gmail.com>
2022-10-09 17:49:34 -0600

    10.9.2

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-import sys, tkinter as tk
+import sys
+from tkinter import *
+from time import strftime
+
+#function to get current time
+def time():
+    string = strftime('%I:%M:%S %p')
+    clock.config(text = string)
+    clock.after(200, time)
 
 # root window
-root=tk.Tk()
-root.title('Pomodoro Timer')
+root=Tk()
+root.title('Clock')
 root.geometry('640x370')
 root.resizable(True,True)
 
 
-greeting = tk.Label(root,text="Hello, Tkinter")
-greeting.pack()
+
+clock = Label(root,font = ('times', 60, 'bold'),pady=25, fg='#CCF381', bg='#4831D4')
+clock.grid(row=2, column=2,pady=100,padx=100)
+#clock.pack(anchor=CENTER)
+time()
 
 # Start the app
 root.mainloop()
