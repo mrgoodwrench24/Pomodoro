d375ac1f2d4c853de261a84740aa35592a993654
michaeltroywhite24 <michaeltroywhite24@gmail.com>
2022-10-09 23:10:08 -0600

    10.9.6

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ from turtle import width
 def time():
     string = strftime('%I:%M:%S %p')
     clock.config(text = string)
-    clock.after(200, time)
+    now = datetime.now()
+    date.config(text=now.strftime('%A %x'))
+    clock.after(1000, time)
+    date.after(1000,time)
 
 # root window
 root=Tk()
@@ -34,12 +37,8 @@ my_notebook.add(my_frame2, text="Pomodoro")
 
 
 clock = Label(my_frame1,font = ('times', 60, 'bold'),fg='#a0b6f7', bg='#404040',pady=40)
-#clock.grid(row=2, column=2,pady=100,padx=100)
-#clock.pack(padx=100,pady=100,expand=True)
 clock.pack(anchor=CENTER)
 date = Label(my_frame1,font = ('times', 30, 'bold'),pady=150, fg='#f2f261', bg='#404040')
-now = datetime.now()
-date.config(text=now.strftime('%A %x'))
 date.pack(anchor=CENTER)
 time()
 
